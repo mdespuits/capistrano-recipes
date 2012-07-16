b860b97f8fba187ea8b3c2868e11fc97df98614d
Matt Bridges <mbridges.91@gmail.com>
2012-07-16 18:25:07 -0500

    updating unicorn recipe to use unicorn_rails for better unicorn management

--- unicorn.rb
+++ unicorn.rb
@@ -1,7 +1,7 @@
 set_default(:unicorn_user) { user }
 set_default(:unicorn_timeout, 30)
 set_default(:unicorn_workers, 1)
-set_default(:unicorn_pid) { "#{current_path}/tmp/pids/unicorn.pid" }
+set_default(:unicorn_pid) { "#{shared_path}/pids/unicorn.pid" }
 set_default(:unicorn_config) { "#{shared_path}/config/unicorn.rb" }
 set_default(:unicorn_err_log) { "#{shared_path}/log/unicorn.stderr.log" }
 set_default(:unicorn_out_log) { "#{shared_path}/log/unicorn.stdout.log" }
@@ -11,20 +11,33 @@ namespace :unicorn do
   desc "Setup Unicorn initializer and app configuration"
   task :setup, roles: :app do
     run "mkdir -p #{shared_path}/config"
+    run "mkdir -p #{shared_path}/pids"
     template "unicorn.rb.erb", unicorn_config
-    template "unicorn_init.erb", "/tmp/unicorn_init"
-    run "chmod +x /tmp/unicorn_init"
-    run "#{sudo} mv /tmp/unicorn_init /etc/init.d/unicorn_#{application}"
-    run "#{sudo} update-rc.d -f unicorn_#{application} defaults"
   end
   after "deploy:setup", "unicorn:setup"
 
-  %w[start stop restart].each do |command|
-    desc "#{command} unicorn"
-    task command, roles: :app do
-      run "service unicorn_#{application} #{command}"
-    end
-    after "deploy:#{command}", "unicorn:#{command}"
+  desc "Symlink unicorn config"
+  task :symlink do
+    run "ln -nfs #{unicorn_config} #{release_path}/config/unicorn.rb"
   end
+  after "deploy:finalize_update", "unicorn:symlink"
+
+  desc "Start Unicorn"
+  task :start, :except => { :no_release => true } do
+    run "cd #{current_path}; bundle exec unicorn_rails -E production -c config/unicorn.rb -D"
+  end
+  after "deploy:start", "unicorn:start"
+
+  desc "Stop Unicorn"
+  task :stop, :except => { :no_release => true } do
+    run "kill -s QUIT `cat #{unicorn_pid}`"
+  end
+  after "deploy:stop", "unicorn:stop"
+
+  desc "Restart unicorn"
+  task :restart, roles: :app do
+    run "kill -s USR2 `cat #{unicorn_pid}`"
+  end
+  after "deploy:restart", "unicorn:restart"
 
 end
