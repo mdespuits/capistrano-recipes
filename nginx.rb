30b37667d052a3ff7836174af1fdc9225e164f5e
Matt Bridges <mbridges.91@gmail.com>
2012-07-17 18:16:56 -0500

    only restart nginx manuall and do not append .copy to nginx.conf in config

--- nginx.rb
+++ nginx.rb
@@ -14,10 +14,9 @@ namespace :nginx do
   desc "Setup nginx configuration for this application"
   task :setup, roles: :web do
     template "nginx.conf.erb", "/tmp/nginx_conf"
-    run "#{sudo} cp /tmp/nginx_conf #{shared_path}/config/nginx.conf.copy"
+    run "#{sudo} cp /tmp/nginx_conf #{shared_path}/config/nginx.conf"
     run "#{sudo} mv /tmp/nginx_conf /etc/nginx/sites-enabled/#{application}"
     run "#{sudo} rm -f /etc/nginx/sites-enabled/default"
-    restart
   end
   after "deploy:setup", "nginx:setup"
   
